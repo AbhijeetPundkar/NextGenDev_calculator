ee2505e8fef3d20120cca794ec51a101f0703219
AbhijeetPundkar <abhijeetpundkar02@gmail.com>
2024-08-15 20:22:58 +0530

    implement calc

---
+++ pages/index.js
@@ -0,0 +1,92 @@
+import Head from 'next/head'
+import { useState } from 'react';
+
+export default function Home() {
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+
+  const ops = ["/", "*", "+", "-", "."];
+
+  const updateCalculator = value => {
+    if(
+      ops.includes(value) && calc === '' || 
+      ops.includes(value) && ops.includes(calc.slice(-1))
+      ) {
+        return;
+      }
+    setCalc(calc + value);
+
+    if(!ops.includes(value)) {
+      setResult(eval(calc + value).toString());
+    }
+  }
+
+  const resetCalculator = () => {
+    setCalc("");
+    setResult("");
+  }
+
+  const digitsButtons = () => {
+    const digits = [];
+  
+    for (let i = 1; i < 10; i++) {
+      digits.push(
+        <button 
+          key={i} 
+          onClick={() => updateCalculator(i.toString())} 
+          className='appearance-none border-0 outline-0 text-gray-300 text-[20px] p-[16px] cursor-pointer transition-transform duration-300 ease-in-out hover:scale-110 hover:bg-gray-800 flex-2 max-w-[33.333%] bg-gray-900'>
+          {i}
+        </button>
+      );
+    }
+    return digits;
+  }
+
+  const calculate = () => {
+      setCalc(eval(calc).toString())
+  }
+
+  return (
+    <>
+    <div className='bg-gray-500 h-screen m-0 p-0 box-border '>
+    <Head>
+        <title>Calculator App</title>
+        <meta name="description" content="Calculator" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      
+    <div className='flex min-h-[100vh] items-center justify-center p-[18px]'>
+      <div className='w-[100%] max-w-[400px] bg-white rounded-[14px] overflow-hidden'>
+      <h1 className='text-center text-[24px] font-bold p-[16px] bg-gray-800 text-white'>Calculator</h1>
+
+        <div className='p-[24px] text-right bg-gray-900 text-gray-300 text-[24px] font-[500]'>
+          { result ? <span className='text-[16px] px-1 text-gray-500 min-w-[324px]'>({ result })</span> : '' } {calc || "0"}
+        </div>
+
+        <div className='flex font-bold'>
+            <button onClick={() => updateCalculator('/')} 
+            className='appearance-none border-0 outline-0 text-gray-300 text-[20px] p-[16px] cursor-pointer transition-transform duration-300 ease-in-out hover:scale-110 hover:bg-[#861616] flex-1 bg-[#a91e1e]'>/</button>
+            <button onClick={() => updateCalculator('*')} 
+            className='appearance-none border-0 outline-0 text-gray-300 text-[20px] p-[16px] cursor-pointer transition-transform duration-300 ease-in-out hover:scale-110 hover:bg-[#861616] flex-1 bg-[#a91e1e]'>*</button>
+            <button onClick={() => updateCalculator('+')} 
+            className='appearance-none border-0 outline-0 text-gray-300 text-[20px] p-[16px] cursor-pointer transition-transform duration-300 ease-in-out hover:scale-110 hover:bg-[#861616] flex-1 bg-[#a91e1e]'>+</button>
+            <button onClick={() => updateCalculator('-')} 
+            className='appearance-none border-0 outline-0 text-gray-300 text-[20px] p-[16px] cursor-pointer transition-transform duration-300 ease-in-out hover:scale-110 hover:bg-[#861616] flex-1 bg-[#a91e1e]'>-</button>
+
+            <button onClick={() => resetCalculator()} className='appearance-none border-0 outline-0 text-gray-300 text-[20px] p-[16px] cursor-pointer transition-transform duration-300 ease-in-out hover:scale-110 hover:bg-[#861616] flex-1 bg-[#a91e1e]'>C</button>
+        </div>
+
+        <div className='flex flex-wrap font-bold'>
+          { digitsButtons() }
+          <button onClick={() => updateCalculator('0')} className='appearance-none border-0 outline-0 text-gray-300 text-[20px] p-[16px] cursor-pointer transition-transform duration-300 ease-in-out hover:scale-110 hover:bg-gray-800 flex-2 max-w-[33.333%] bg-gray-900'>0</button>
+          <button onClick={() => updateCalculator('.')} className='appearance-none border-0 outline-0 text-gray-300 text-[20px] p-[16px] cursor-pointer transition-transform duration-300 ease-in-out hover:scale-110 hover:bg-gray-800 flex-2 max-w-[33.333%] bg-gray-900'>.</button>
+          <button onClick={() => calculate()} className='appearance-none border-0 outline-0 text-gray-300 text-[20px] p-[16px] cursor-pointer transition-transform duration-300 ease-in-out hover:scale-110 hover:bg-gray-800 flex-2 max-w-[33.333%] bg-gray-900'>
+            =
+            </button>
+        </div>
+      </div>
+    </div>
+    </div>
+    </>
+  )
+}
